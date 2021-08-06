02eb96ef7562adb44dcce3a68998fe6cb017e788
TanXiCai <1666941977@qq.com>
2021-08-06 21:25:33 +0800

    kannh项目第一次提交

---
+++ bdmdw/src/main/java/com/kannh/bdmdw/BdmdwApplication.java
@@ -0,0 +1,13 @@
+package com.kannh.bdmdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdmdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdmdwApplication.class, args);
+	}
+
+}
---
+++ bdmdw/src/test/java/com/kannh/bdmdw/BdmdwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kannh.bdmdw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdmdwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
